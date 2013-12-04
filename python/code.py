fc67eea817920765a611222e2ba6bb1c5305f343
coopaloopster <pjc370@nyu.edu>
2013-12-04 01:19:11 -0500

    Hi Guys!  I'm new here ;)

--- python/code.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import numexpr
-
-sig = numexpr.NumExpr("1.0/(1.0+exp(-z))")
-sizes = [4, 3, 3]
-biases = [np.random.randn(y, 1) for y in sizes[1:]]
-weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
