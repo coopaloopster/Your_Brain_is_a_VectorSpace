05e7f34108acfebaecc920e37eefd513a6355b34
elju <moresilenter@gmail.com>
2013-12-13 22:37:19 -0500

    Beat Haskell into IO submission

--- haskell/peoplearecomputers.hs
+++ haskell/peoplearecomputers.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Data.List
+import qualified Data.Map as Map
+import Data.Aeson
+import Control.Applicative
+import Data.Functor
+import Control.Monad
+import qualified Data.ByteString.Lazy as BL
 
-main = print (mmult [[1,2], [3,4]] [[5,6], [1,3]])
+data TrainingData = TD
+    { flower :: Map.Map String Float, training_data :: [[Float]] } deriving (Show)
+
+instance FromJSON TrainingData where
+    parseJSON (Object v) = TD <$>
+                        v .: "flower" <*>
+                        v .: "training_data"
 
 mmult :: Num a => [[a]] -> [[a]] -> [[a]]
 mmult a b = [ [ sum $ zipWith (*) ar bc | bc <- (transpose b) ] | ar <- a ]
+
+main :: IO()
+main = do
+    a <- BL.readFile "../data.json"
+    (Just b) <- return (decode a :: Maybe TrainingData)
+    print b
